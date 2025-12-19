bdec5fe1f2a9031e24b4140eb0f205a818ca67fa
TranDatk <hgooshvhd123@gmail.com>
2025-12-19 18:31:10 +0700

    Add Update method and query timeouts to Postgres repo
    Introduces an Update method to the PostgresRepository for updating posts. Adds a constant for query timeout duration and applies context timeouts to GetAll and GetByID methods to improve query reliability. Also adds a new (empty) seed.go file.

--- internal/posts/repository/postgres.go
+++ internal/posts/repository/postgres.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"database/sql"
 	"social/internal/entity"
+	"time"
 
 	"github.com/lib/pq"
 )
+
+const QueryTimeoutDuration = 5 * time.Second
 
 type PostgresRepository struct {
 	DB *sql.DB
@@ -34,7 +37,33 @@ func (r *PostgresRepository) Create(ctx context.Context, p *entity.Post) error {
 	return nil
 }
 
+func (r *PostgresRepository) Update(ctx context.Context, p *entity.Post) error {
+	query := `
+		UPDATE posts
+		SET
+			title = $1,
+			content = $2,
+			tags = $3
+		WHERE id = $4
+		AND updated_at = $5
+		RETURNING id, title, content, user_id, tags
+	`
+
+	err := r.DB.QueryRowContext(ctx, query, p.ID, p.Title, p.Content, p.UserID, pq.Array(p.Tags)).Scan(
+		&p.ID, &p.Title, &p.Content, &p.UserID, pq.Array(&p.Tags),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (r *PostgresRepository) GetAll(ctx context.Context) (*[]entity.Post, error) {
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
 	query := `
 		SELECT id, title, content, user_id, tags
 		FROM posts
@@ -71,6 +100,8 @@ func (r *PostgresRepository) GetAll(ctx context.Context) (*[]entity.Post, error)
 
 func (r *PostgresRepository) GetByID(ctx context.Context, id string) (*entity.Post, error) {
 	var p entity.Post
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
 
 	err := r.DB.QueryRowContext(ctx,
 		"SELECT id, title, content, user_id, tags FROM posts WHERE id=$1", id).
